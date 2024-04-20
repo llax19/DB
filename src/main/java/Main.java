74a3bbd727da0b44f7550c8b5ad74ad3b0e64288
llaxx19 <1634055948@qq.com>
2024-04-20 10:15:34 +0800

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,19 @@
 import utils.ConnectConfig;
 import utils.DatabaseConnector;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.logging.Logger;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.Headers;
+
 
 public class Main {
 
     private static final Logger log = Logger.getLogger(Main.class.getName());
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             // parse connection config from "resources/application.yaml"
             ConnectConfig conf = new ConnectConfig();
@@ -20,7 +26,19 @@ public class Main {
                 System.exit(1);
             }
             /* do somethings */
-            
+            // 创建HTTP服务器，监听指定端口
+            // 这里是8000，建议不要80端口，容易和其他的撞
+            HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+            // 添加handler，这里就绑定到/card路由
+            // 所以localhost:8000/card是会有handler来处理
+            server.createContext("/card", new CardHandler());
+
+            // 启动服务器
+            server.start();
+
+            // 标识一下，这样才知道我的后端启动了（确信
+            System.out.println("Server is listening on port 8000");
             // release database connection handler
             if (connector.release()) {
                 log.info("Success to release connection.");
@@ -31,5 +49,32 @@ public class Main {
             e.printStackTrace();
         }
     }
+    static class CardHandler implements HttpHandler {
+        // 关键重写handle方法
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            // 允许所有域的请求，cors处理
+            Headers headers = exchange.getResponseHeaders();
+            headers.add("Access-Control-Allow-Origin", "*");
+            headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+            headers.add("Access-Control-Allow-Headers", "Content-Type");
+            // 解析请求的方法，看GET还是POST
+            String requestMethod = exchange.getRequestMethod();
+            // 注意判断要用equals方法而不是==啊，java的小坑（
+            if (requestMethod.equals("GET")) {
+                // 处理GET
+                handleGetRequest(exchange);
+            } else if (requestMethod.equals("POST")) {
+                // 处理POST
+                handlePostRequest(exchange);
+            } else if (requestMethod.equals("OPTIONS")) {
+                // 处理OPTIONS
+                handleOptionsRequest(exchange);
+            } else {
+                // 其他请求返回405 Method Not Allowed
+                exchange.sendResponseHeaders(405, -1);
+            }
+        }
+}
 
 }
